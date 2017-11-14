ed1661c77f5a107c67bf2384bcd0e3e255a19ede
quentin <quentin@bannerman.com>
2017-11-14 23:16:33 +0100

    impr: fix itemSpriteToPlace not being reseted + clean

--- src/ItemsBar.ts
+++ src/ItemsBar.ts
@@ -1,17 +1,18 @@
-import {Item} from "./Item";
-import {Level} from "./Level";
+import { boxContains } from './helpers';
+import { Item } from './Item';
+import { Level } from './Level';
 
 declare var game: Phaser.Game;
 declare var level: Level;
 
-var timeStarted =0;
+let timeStarted = 0;
 
 export interface ItemToPlace {
-	item: typeof Item,
-	available: number,
-	count?: number,
-	button?: Phaser.Button,
-	textCount?: Phaser.Text
+	item: typeof Item;
+	available: number;
+	count?: number;
+	button?: Phaser.Button;
+	textCount?: Phaser.Text;
 	key?: string;
 }
 
@@ -20,17 +21,17 @@ export class ItemsBar {
 	selectedItem: ItemToPlace = null;
 	itemSpriteToPlace: Phaser.Sprite;
 
-	constructor(public items: Array<ItemToPlace> = []){
+	constructor(public items: Array<ItemToPlace> = []) {
 		timeStarted = Date.now();
 
 		game.add.sprite(1120, 0, 'items_bar');
 
-		const playButton = game.add.sprite(1144,12, 'button_play');
-		const undoButton = game.add.sprite(1204,12, 'button_undo');
+		const playButton = game.add.sprite(1144, 12, 'button_play');
+		const undoButton = game.add.sprite(1204, 12, 'button_undo');
 
 		playButton.inputEnabled = true;
 		playButton.events.onInputDown.add(e => {
-			if(game.paused){
+			if (game.paused) {
 				playButton.loadTexture('button_restart');
 				game.paused = false;
 			} else {
@@ -46,78 +47,91 @@ export class ItemsBar {
 			game.paused = true;
 			level.reset();
 			this.resetItems();
-		})
+		});
 
-		this.timeText = game.add.text(0, 0, "", {
-			font: "24px Arial",
-			fill : "#fff",
-			boundsAlignH: "center",
-			boundsAlignV: "middle"
-		})
-		this.timeText.setTextBounds(1151,104,96,32)
+		this.timeText = game.add.text(0, 0, '', {
+			font: '24px Arial',
+			fill: '#fff',
+			boundsAlignH: 'center',
+			boundsAlignV: 'middle'
+		});
+		this.timeText.setTextBounds(1151, 104, 96, 32);
 
 		this.updateTime();
 		setInterval(() => this.updateTime(), 1000);
 
 		this.items.forEach((itemToPlace, i) => {
 			itemToPlace.key = (new itemToPlace.item()).key;
-			itemToPlace.button = game.add.button(1170,265 + i * 80, itemToPlace.key);
-			itemToPlace.button.anchor.set(0.5,0.5);
+			itemToPlace.button = game.add.button(1170, 265 + i * 80, itemToPlace.key);
+			itemToPlace.button.anchor.set(0.5, 0.5);
 			itemToPlace.button.tint = 0xCCCCCC;
-			itemToPlace.button.onInputDown.add(() => this.selectItem(itemToPlace))
-			itemToPlace.textCount = game.add.text(1220, 250 + i * 80, `x ${itemToPlace.available}`,  {
-				font: "24px Arial",
-				fill : "#fff",
-				boundsAlignH: "center",
-				boundsAlignV: "middle"
-			})
+			itemToPlace.button.onInputDown.add(() => this.selectItem(itemToPlace));
+			itemToPlace.textCount = game.add.text(1220, 250 + i * 80, `x ${itemToPlace.available}`, {
+				font: '24px Arial',
+				fill: '#fff',
+				boundsAlignH: 'center',
+				boundsAlignV: 'middle'
+			});
 		});
 
 		this.resetItems();
 		this.initInput();
 	}
 
-	resetItems(){
+	resetItems() {
 		this.unselectItem();
-		for(let itemToPlace of this.items){
+		for (const itemToPlace of this.items) {
 			itemToPlace.count = itemToPlace.available;
-			itemToPlace.textCount.text = `x ${itemToPlace.count}`
+			itemToPlace.textCount.text = `x ${itemToPlace.count}`;
 		}
 	}
 
-	selectItem(itemToPlace: ItemToPlace){
-		if(!game.paused || itemToPlace.count <= 0) return;
+	selectItem(itemToPlace: ItemToPlace) {
+		if (!game.paused || itemToPlace.count <= 0) return;
 		this.unselectItem();
 		this.selectedItem = itemToPlace;
 		this.selectedItem.button.tint = 0xFFFFFF;
 
-		if(this.itemSpriteToPlace) this.itemSpriteToPlace.destroy();
-		this.itemSpriteToPlace = game.add.sprite(0,0, itemToPlace.key);
+		if (this.itemSpriteToPlace) this.itemSpriteToPlace.destroy();
+		this.itemSpriteToPlace = game.add.sprite(0, 0, itemToPlace.key);
 		this.itemSpriteToPlace.anchor.set(0.5, 0.5);
 		this.itemSpriteToPlace.visible = false;
 	}
 
-	unselectItem(){
-		if(this.selectedItem) this.selectedItem.button.tint = 0xCCCCCC;
-		if(this.itemSpriteToPlace) this.itemSpriteToPlace.destroy();
+	unselectItem() {
+		if (this.selectedItem) this.selectedItem.button.tint = 0xCCCCCC;
+		if (this.itemSpriteToPlace) this.itemSpriteToPlace.destroy();
 		this.selectedItem = null;
+		this.itemSpriteToPlace = null;
 	}
 
-	updateTime(){
+	spawnItem(item: ItemToPlace, x: number, y: number) {
+		const newItem = new item.item();
+		const playerSprite = newItem.spawn(x, y);
+		level.playerItems.push({
+			item: newItem,
+			position: { x, y },
+			sprite: playerSprite
+		});
+
+		item.count--;
+		item.textCount.text = `x ${item.count}`;
+	}
+
+	updateTime() {
 		const time = new Date(Date.now() - timeStarted);
-		let [m, s] = [time.getMinutes(), time.getSeconds()]
-		this.timeText.text = `${m<10 ? '0'+m : m} : ${s<10 ? '0'+s : s}`
+		const [m, s] = [time.getMinutes(), time.getSeconds()];
+		this.timeText.text = `${m < 10 ? '0' + m : m} : ${s < 10 ? '0' + s : s}`;
 	}
 
-	initInput(){
+	initInput() {
 		game.input.addMoveCallback((pointer, x, y) => {
-			if(this.itemSpriteToPlace && game.paused){
-				const {width, height} = this.itemSpriteToPlace;
-				if(x > width/2
-				&& x < (game.width - 160 - width/2)
-				&& y > (64 + height/2)
-				&& y < game.height - height/2
-				){
+			if (this.itemSpriteToPlace && game.paused) {
+				const { width, height } = this.itemSpriteToPlace;
+				if (boxContains([
+					[width / 2, 64 + height / 2],
+					[game.width - 160 - width / 2, game.height - height / 2]
+				], [x, y])) {
 					this.itemSpriteToPlace.visible = true;
 					this.itemSpriteToPlace.x = x;
 					this.itemSpriteToPlace.y = y;
@@ -128,25 +142,15 @@ export class ItemsBar {
 		}, this);
 
 		game.input.onTap.add(event => {
-			if(this.itemSpriteToPlace && game.paused){
-				let {x,y} = event;
-				const {width, height} = this.itemSpriteToPlace;
+			if (this.itemSpriteToPlace && game.paused) {
+				const { x, y } = event;
+				const { width, height } = this.itemSpriteToPlace;
 
-				if(x > width/2
-				&& x < (game.width - 160 - width/2)
-				&& y > (64 + height/2)
-				&& y < game.height - height/2
-				){
-					const newItem = new this.selectedItem.item();
-					const playerSprite = newItem.spawn(x,y);
-					level.playerItems.push({
-						item: newItem,
-						position: {x,y},
-						sprite: playerSprite
-					});
-
-					this.selectedItem.count--;
-					this.selectedItem.textCount.text = `x ${this.selectedItem.count}`
+				if (boxContains([
+					[width / 2, 64 + height / 2],
+					[game.width - 160 - width / 2, game.height - height / 2]
+				], [x, y])) {
+					this.spawnItem(this.selectedItem, x, y);
 					this.unselectItem();
 				}
 			}
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,44 +1,51 @@
 declare var game: Phaser.Game;
 
-export enum DIRECTION { LEFT, RIGHT };
-export enum DIRECTION4 { LEFT, RIGHT, TOP, BOTTOM };
-export enum COLOR { RED, GREEN, BLUE, YELLOW };
+export enum DIRECTION { LEFT, RIGHT }
+export enum DIRECTION4 { LEFT, RIGHT, TOP, BOTTOM }
+export enum COLOR { RED, GREEN, BLUE, YELLOW }
 
-export const extractContactPoint = handler => function(bodyA,bodyB,shapeA,shapeB,equation: p2.ContactEquation[]){
-	let pos = equation[0].bodyA.position;
-	let pt = equation[0].contactPointA;
+export const extractContactPoint = handler => function (bodyA, bodyB, shapeA, shapeB, equation: p2.ContactEquation[]) {
+	const pos = equation[0].bodyA.position;
+	const pt = equation[0].contactPointA;
 
-	let cx = game.physics.p2.mpxi(pos[0] + pt[0]);
-	let cy = game.physics.p2.mpxi(pos[1] + pt[1]);
+	const cx = game.physics.p2.mpxi(pos[0] + pt[0]);
+	const cy = game.physics.p2.mpxi(pos[1] + pt[1]);
 
-	return handler.call(this, { x: cx, y: cy })
+	return handler.call(this, { x: cx, y: cy });
+};
+
+export function removeInArray(arr, elm) {
+	arr.splice(arr.indexOf(elm), 1);
+	return arr;
 }
 
-export function removeInArray(arr,elm){
-	arr.splice(arr.indexOf(elm), 1)
-	return arr
+export function removeAllInArray(arr, elm) {
+	return arr.filter(e => e !== elm);
 }
 
-export function removeAllInArray(arr,elm){
-	return arr.filter(e => e!== elm)
+export function inArray(arr, elm) {
+	return arr.indexOf(elm) >= 0; // TS, donne moi includes() !!!
 }
 
-export function inArray(arr, elm){
-	return arr.indexOf(elm) >= 0 // TS, donne moi includes() !!!
-}
-
-export function overlap(sprite, rect){
-	let bounds = sprite.getBounds();
+export function overlap(sprite, rect) {
+	const bounds = sprite.getBounds();
 	return bounds.x + bounds.width > rect.x
 		&& bounds.x < rect.x + rect.width
 		&& bounds.y + bounds.height > rect.y
-		&& bounds.y < rect.y + rect.height
+		&& bounds.y < rect.y + rect.height;
 }
 
-export function calcDistance(x1, y1, x2, y2){
-	return Math.sqrt( Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2) );
+export function calcDistance(x1, y1, x2, y2) {
+	return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
 }
 
-export function calcAngle(x1, y1, x2, y2){
+export function calcAngle(x1, y1, x2, y2) {
 	return Math.atan2(y2 - y1, x2 - x1);
+}
+
+export function boxContains([NW, SE]: number[][], [x, y]: number[], strict = true) {
+	return x > NW[0]
+		&& x < SE[0]
+		&& y > NW[1]
+		&& y < SE[1];
 }
